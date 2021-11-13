c7346ca4416bc291a6f032e3f442157e183479b9
xu yuanteng <13883986114@163.com>
2021-11-13 19:38:23 +0800

    combined version

--- utils/db.go
+++ utils/db.go
@@ -10,8 +10,8 @@ var (
 )
 
 func InitMySql() (err error) {
-	//修改为自己的数据库密码
-	dsn := "root:112757995@(127.0.0.1:3306)/appUpgrade?charset=utf8mb4&parseTime=True&loc=Local"
+	//修改为自己的数据库密码和数据库名
+	dsn := "root:123456@(127.0.0.1:3306)/appUpgrade?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return err
 }
